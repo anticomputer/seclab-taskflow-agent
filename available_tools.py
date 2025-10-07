0b7440fad6317f01abf8b2e46de1f618e454a8d5
Kevin Backhouse <kevinbackhouse@github.com>
2025-10-07 17:45:54 +0100

    Add docstring

--- available_tools.py
+++ available_tools.py
@@ -1,4 +1,8 @@
 class AvailableTools:
+    """
+    This class is used for storing dictionaries of all the available
+    personalities, taskflows, and prompts.
+    """
     def __init__(self, personalities: dict, taskflows: dict, prompts: dict):
         self.personalities = personalities
         self.taskflows = taskflows
